15b87439a9adcf333f6c0bc89aba08acf6152e0d
SheLovesCode <munyaradzi.diana@gmail.com>
2020-05-15 23:05:45 +0200

    Changed frameworks

--- invertedIndex.py
+++ invertedIndex.py
@@ -1,28 +1,38 @@
-# Import mrjob, mrstep and regular expressions modules
-from mrjob.job  import MRJob
-from mrjob.step import MRStep
+# Import string, mrs and regular expressions modules
 import re
+import mrs
+import string
 
-# Compiles regular expression pattern into a reusable regular expression object
-WORD_RE = re.compile(r"[\w']+")
+# Extract stop words from textfile and store in a list
+stopWords = open('stopWords.txt','r').read().split()
 
-class invertedIndexing(MRJob):
-    def steps(self):
-        return [MRStep(mapper = self.mapper_word_index, 
-                        reducer = self.reducer_word_index_list)]
+class invertedIndexing(mrs.MapReduce):
     
-    # Mapper takes in a key, which in this case is NONE, and a value, which is a line 
-    def mapper_word_index(self, _, line):
-        indexNumber, line = line.split(" ", 1) # Reads a line, extracts the indexNumber and creates list of the words in the line
-        for word in WORD_RE.findall(line):
-            if not word.isdigit(): # Only proceeds once word has been validated that it is not a number
-               yield(word.lower(), indexNumber)  # Returns lowercase word and the index (line) number
+    # Mapper takes in a key, which is the indexNumber and a value, which is a line 
+    def map(self, indexNumber, line):
+        # Reads a line and creates list of the words in the line
+        line = line.split() 
+        for word in line:
+            # Converts all letters to lowercase
+            word = word.lower()
+            # Converts every instance of punctuation to a space
+            word = word.translate(str.maketrans(string.punctuation, ' '*len(string.punctuation)))
+            #Only maps word once it has been validated that it only contains the alphabet, it's larger than 3 letters and is not a stopword
+            if word.isalpha() and len(word) > 3 and word not in stopWords: 
+               yield(word, indexNumber + 1)  # Returns lowercase word, stripped of all punstuation and the index number
 
-    # Reducer takes in the key, which is a word, and a value, which is the index (line) number
-    def reducer_word_index_list(self, word, indexNumbers):
-        indexNumbersList = list(indexNumbers) # Creates a list of the index numbers
-        yield(word.lower(), indexNumbersList)
+    # Reducer takes in the key, which is a word, and a value, which is the index number and returns the list of indexNumbers
+    def reduce(self, word, indexNumber):
+        indexNumbers = []
+
+        for currentIndex in indexNumber:
+            # If statements to ensure only the first 50 lines are recorded and that if a word appears twice on one line, its only recorded once
+            if len(indexNumbers) >= 50:
+                break
+            if currentIndex not in indexNumbers:
+                indexNumbers.append(currentIndex)       
+        yield(indexNumbers)
 
 if __name__ == '__main__':
-	invertedIndexing.run()
+	mrs.main(invertedIndexing)
 
